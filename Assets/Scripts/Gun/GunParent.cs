eab65c94d4df407a1db90b1cfb5e92cd9b62917a
Jacrys3 <ryse0528@gmail.com>
2022-11-06 21:43:50 -0800

    added dale animations and arm

--- Assets/Scripts/Gun/GunParent.cs
+++ Assets/Scripts/Gun/GunParent.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class GunParent : MonoBehaviour
 {
+
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Dale");
+    }
+
     private void Update()
     {
         Vector3 mousePos = Input.mousePosition;
@@ -11,15 +19,22 @@ public class GunParent : MonoBehaviour
         transform.right = (mousePosition-(Vector2)transform.position).normalized;
         
         float rotZ = transform.rotation.eulerAngles.z;
+
         Vector3 change = Vector3.one;
+        Vector3 playerChange = Vector3.one;
         if (rotZ > 90 && rotZ < 270)
         {
+            change.x = -1f;
             change.y = -1f;
+            playerChange.x = -1f;
         }
         else
         {
+            change.x = 1f;
             change.y = 1f;
+            playerChange.x = 1f;
         }
         transform.localScale = change;
+        player.transform.localScale = playerChange;
     }
 }

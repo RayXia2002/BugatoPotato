a72ae7c2542de1a47d0dc1ba51452ba0bfa611ae
Jacrys3 <ryse0528@gmail.com>
2022-11-16 12:31:04 -0800

    fixed tutorial

--- Assets/Scripts/TutorialController.cs
+++ Assets/Scripts/TutorialController.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TutorialController : MonoBehaviour
 {
 
     public DayNightController dnc;
     public Image[] images;
+    public TMP_Text[] texts;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +22,12 @@ public class TutorialController : MonoBehaviour
         if(dnc.getDays() == 1)
         {
             updateImages(true);
+            updateTexts(true);
         }
         else
         {
             updateImages(false);
+            updateTexts(false);
         }
     }
 
@@ -32,6 +36,14 @@ public class TutorialController : MonoBehaviour
         for (int i = 0; i < 4; i++)
         {
             images[i].enabled = onOff;
+        }
+    }
+
+    void updateTexts(bool onOff)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            texts[i].enabled = onOff;
         }
     }
 }
